87a49f0966c5bdb5e37a078e4d006f0a321d4265
Kayvon <kayvonwallace@gmail.com>
2024-09-05 08:40:42 -0700

    Added additional console logs inside on promise wi th the then() method to access out of scoop variab les.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 
 const question = "Can you tell me my future?"
 
-const promise = tell(question);
+const tellPromise = tell(question);
 
-console.log(promise);
-promise.then(console.log)
+console.log(tellPromise);
+tellPromise.then((fortune) => {
+    console.log(question);
+    console.log(fortune);
+  });
